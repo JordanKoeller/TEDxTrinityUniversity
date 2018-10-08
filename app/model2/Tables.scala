bc24dea2426625e42c1785e07eb413f9449fd35e
JordanKoeller <jkoeller12@gmail.com>
2018-10-08 14:59:16 -0500

    I think DB should be happy now

--- app/model2/Tables.scala
+++ app/model2/Tables.scala
@@ -1,10 +1,5 @@
 package model2
 // AUTO-GENERATED Slick data model
-
-/* SOME CODE I AM ADDING */
-
-
-
 /** Stand-alone Slick data model for immediate use */
 object Tables extends {
   val profile = slick.jdbc.PostgresProfile
@@ -24,7 +19,7 @@ trait Tables {
   def ddl = schema
 
   /** Entity class storing rows of table Event
-   *  @param id Database column id SqlType(int4), PrimaryKey
+   *  @param id Database column id SqlType(serial), AutoInc, PrimaryKey
    *  @param title Database column title SqlType(varchar), Length(1000,true)
    *  @param subtitle Database column subtitle SqlType(varchar), Length(1000,true), Default(None)
    *  @param description Database column description SqlType(text)
@@ -35,7 +30,7 @@ trait Tables {
    *  @param takenSeats Database column taken_seats SqlType(int4)
    *  @param registrationLink Database column registration_link SqlType(varchar), Length(1000,true)
    *  @param mediaLink Database column media_link SqlType(varchar), Length(1000,true), Default(None) */
-  case class EventRow(override val id: Int, title: String, subtitle: Option[String] = None, description: String, venue: String, eventDate: java.sql.Date,override val time: java.sql.Time, maxSeats: Int, takenSeats: Int, registrationLink: String, mediaLink: Option[String] = None) extends Post(eventDate,time,id)
+  case class EventRow(id: Int, title: String, subtitle: Option[String] = None, description: String, venue: String, eventDate: java.sql.Date, time: java.sql.Time, maxSeats: Int, takenSeats: Int, registrationLink: String, mediaLink: Option[String] = None)
   /** GetResult implicit for fetching EventRow objects using plain SQL queries */
   implicit def GetResultEventRow(implicit e0: GR[Int], e1: GR[String], e2: GR[Option[String]], e3: GR[java.sql.Date], e4: GR[java.sql.Time]): GR[EventRow] = GR{
     prs => import prs._
@@ -47,8 +42,8 @@ trait Tables {
     /** Maps whole row to an option. Useful for outer joins. */
     def ? = (Rep.Some(id), Rep.Some(title), subtitle, Rep.Some(description), Rep.Some(venue), Rep.Some(eventDate), Rep.Some(time), Rep.Some(maxSeats), Rep.Some(takenSeats), Rep.Some(registrationLink), mediaLink).shaped.<>({r=>import r._; _1.map(_=> EventRow.tupled((_1.get, _2.get, _3, _4.get, _5.get, _6.get, _7.get, _8.get, _9.get, _10.get, _11)))}, (_:Any) =>  throw new Exception("Inserting into ? projection not supported."))
 
-    /** Database column id SqlType(int4), PrimaryKey */
-    val id: Rep[Int] = column[Int]("id", O.PrimaryKey)
+    /** Database column id SqlType(serial), AutoInc, PrimaryKey */
+    val id: Rep[Int] = column[Int]("id", O.AutoInc, O.PrimaryKey)
     /** Database column title SqlType(varchar), Length(1000,true) */
     val title: Rep[String] = column[String]("title", O.Length(1000,varying=true))
     /** Database column subtitle SqlType(varchar), Length(1000,true), Default(None) */
@@ -74,7 +69,7 @@ trait Tables {
   lazy val Event = new TableQuery(tag => new Event(tag))
 
   /** Entity class storing rows of table NewsletterPost
-   *  @param id Database column id SqlType(int4), PrimaryKey
+   *  @param id Database column id SqlType(serial), AutoInc, PrimaryKey
    *  @param postDate Database column post_date SqlType(date)
    *  @param description Database column description SqlType(text)
    *  @param userId Database column user_id SqlType(int4)
@@ -95,8 +90,8 @@ trait Tables {
     /** Maps whole row to an option. Useful for outer joins. */
     def ? = (Rep.Some(id), Rep.Some(postDate), Rep.Some(description), Rep.Some(userId), Rep.Some(title), Rep.Some(subtitle), Rep.Some(`abstract`), mediaLink).shaped.<>({r=>import r._; _1.map(_=> NewsletterPostRow.tupled((_1.get, _2.get, _3.get, _4.get, _5.get, _6.get, _7.get, _8)))}, (_:Any) =>  throw new Exception("Inserting into ? projection not supported."))
 
-    /** Database column id SqlType(int4), PrimaryKey */
-    val id: Rep[Int] = column[Int]("id", O.PrimaryKey)
+    /** Database column id SqlType(serial), AutoInc, PrimaryKey */
+    val id: Rep[Int] = column[Int]("id", O.AutoInc, O.PrimaryKey)
     /** Database column post_date SqlType(date) */
     val postDate: Rep[java.sql.Date] = column[java.sql.Date]("post_date")
     /** Database column description SqlType(text) */
@@ -117,7 +112,7 @@ trait Tables {
   lazy val NewsletterPost = new TableQuery(tag => new NewsletterPost(tag))
 
   /** Entity class storing rows of table Posters
-   *  @param id Database column id SqlType(int4), PrimaryKey
+   *  @param id Database column id SqlType(serial), AutoInc, PrimaryKey
    *  @param name Database column name SqlType(varchar), Length(100,true)
    *  @param email Database column email SqlType(varchar), Length(40,true) */
   case class PostersRow(id: Int, name: String, email: String)
@@ -132,8 +127,8 @@ trait Tables {
     /** Maps whole row to an option. Useful for outer joins. */
     def ? = (Rep.Some(id), Rep.Some(name), Rep.Some(email)).shaped.<>({r=>import r._; _1.map(_=> PostersRow.tupled((_1.get, _2.get, _3.get)))}, (_:Any) =>  throw new Exception("Inserting into ? projection not supported."))
 
-    /** Database column id SqlType(int4), PrimaryKey */
-    val id: Rep[Int] = column[Int]("id", O.PrimaryKey)
+    /** Database column id SqlType(serial), AutoInc, PrimaryKey */
+    val id: Rep[Int] = column[Int]("id", O.AutoInc, O.PrimaryKey)
     /** Database column name SqlType(varchar), Length(100,true) */
     val name: Rep[String] = column[String]("name", O.Length(100,varying=true))
     /** Database column email SqlType(varchar), Length(40,true) */
@@ -143,7 +138,7 @@ trait Tables {
   lazy val Posters = new TableQuery(tag => new Posters(tag))
 
   /** Entity class storing rows of table Speakers
-   *  @param speakerId Database column speaker_id SqlType(int4), PrimaryKey
+   *  @param speakerId Database column speaker_id SqlType(serial), AutoInc, PrimaryKey
    *  @param eventId Database column event_id SqlType(int4)
    *  @param name Database column name SqlType(varchar), Length(1000,true)
    *  @param bio Database column bio SqlType(text)
@@ -160,8 +155,8 @@ trait Tables {
     /** Maps whole row to an option. Useful for outer joins. */
     def ? = (Rep.Some(speakerId), Rep.Some(eventId), Rep.Some(name), Rep.Some(bio), Rep.Some(media)).shaped.<>({r=>import r._; _1.map(_=> SpeakersRow.tupled((_1.get, _2.get, _3.get, _4.get, _5.get)))}, (_:Any) =>  throw new Exception("Inserting into ? projection not supported."))
 
-    /** Database column speaker_id SqlType(int4), PrimaryKey */
-    val speakerId: Rep[Int] = column[Int]("speaker_id", O.PrimaryKey)
+    /** Database column speaker_id SqlType(serial), AutoInc, PrimaryKey */
+    val speakerId: Rep[Int] = column[Int]("speaker_id", O.AutoInc, O.PrimaryKey)
     /** Database column event_id SqlType(int4) */
     val eventId: Rep[Int] = column[Int]("event_id")
     /** Database column name SqlType(varchar), Length(1000,true) */
@@ -175,7 +170,7 @@ trait Tables {
   lazy val Speakers = new TableQuery(tag => new Speakers(tag))
 
   /** Entity class storing rows of table TeamMember
-   *  @param id Database column id SqlType(int4)
+   *  @param id Database column id SqlType(serial), AutoInc, PrimaryKey
    *  @param isActive Database column is_active SqlType(int4)
    *  @param name Database column name SqlType(varchar), Length(1000,true)
    *  @param position Database column position SqlType(varchar), Length(1000,true)
@@ -184,7 +179,7 @@ trait Tables {
    *  @param biography Database column biography SqlType(text)
    *  @param email Database column email SqlType(varchar), Length(1000,true)
    *  @param mediaUrl Database column media_url SqlType(varchar), Length(1000,true) */
-  case class TeamMemberRow(override val id: Int, isActive: Int, name: String, position: String, major: String, gradClass: Int, biography: String, email: String, mediaUrl: String) extends WithID(id)
+  case class TeamMemberRow(id: Int, isActive: Int, name: String, position: String, major: String, gradClass: Int, biography: String, email: String, mediaUrl: String)
   /** GetResult implicit for fetching TeamMemberRow objects using plain SQL queries */
   implicit def GetResultTeamMemberRow(implicit e0: GR[Int], e1: GR[String]): GR[TeamMemberRow] = GR{
     prs => import prs._
@@ -196,8 +191,8 @@ trait Tables {
     /** Maps whole row to an option. Useful for outer joins. */
     def ? = (Rep.Some(id), Rep.Some(isActive), Rep.Some(name), Rep.Some(position), Rep.Some(major), Rep.Some(gradClass), Rep.Some(biography), Rep.Some(email), Rep.Some(mediaUrl)).shaped.<>({r=>import r._; _1.map(_=> TeamMemberRow.tupled((_1.get, _2.get, _3.get, _4.get, _5.get, _6.get, _7.get, _8.get, _9.get)))}, (_:Any) =>  throw new Exception("Inserting into ? projection not supported."))
 
-    /** Database column id SqlType(int4) */
-    val id: Rep[Int] = column[Int]("id")
+    /** Database column id SqlType(serial), AutoInc, PrimaryKey */
+    val id: Rep[Int] = column[Int]("id", O.AutoInc, O.PrimaryKey)
     /** Database column is_active SqlType(int4) */
     val isActive: Rep[Int] = column[Int]("is_active")
     /** Database column name SqlType(varchar), Length(1000,true) */
--- app/util/DatabaseCodeGen.scala
+++ app/util/DatabaseCodeGen.scala
@@ -4,7 +4,7 @@ object DatabaseCodeGen extends App {
   slick.codegen.SourceCodeGenerator.main(
       Array("slick.jdbc.PostgresProfile", "org.postgresql.Driver",
           "jdbc:postgresql://ec2-54-235-148-19.compute-1.amazonaws.com:5432/dcegnlpg1iu06?user=wlpnclrrhqlewa&password=5b84c825241693cc64516a7456186712d78c4eb6779aad3948d42f7298608fc0&sslmode=require",
-          "/home/raqmu/Desktop",
+          "/home/raqmu/Documents",
           "model2"
           )
       )
